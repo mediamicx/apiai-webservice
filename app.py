549c693d4cd63ddf08c90105495c8b6ccdf98a1e
Michael Schultheiß <michael.schultheiss@zeit.de>
2016-11-02 14:34:40 +0100

    added basic response to the webservice

--- app.py
+++ app.py
@@ -28,15 +28,17 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
+    if req.get("result").get("action") != "report.bug":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    link = parameters.get("Link")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    # cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    # speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+
+    speech = "Webhook ok"
 
     print("Response:")
     print(speech)
@@ -46,7 +48,7 @@ def makeWebhookResult(req):
         "displayText": speech,
         #"data": {},
         # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
+        "source": "apiai-webservice"
     }
 
 
